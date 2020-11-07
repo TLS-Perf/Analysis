456f258c8b9cc6840f88f56478a66a6dfc3a89f4
Leonard1874 <leonardwang2018@gmail.com>
2020-11-07 00:00:44 -0500

    ww:num

--- tool.py
+++ tool.py
@@ -1,6 +1,6 @@
 import os
 
-num = 100 #number of websites
+num = 3 #number of websites
 
 def readcsv(website):
     # Using readline() 
@@ -23,16 +23,48 @@ def runcurl(websites, outputs):
     for website in websites:
         print(website)
         stream = os.popen('./curltimes.sh compare https://' + website)
+        #print(stream.read())
         outputs.append(stream.read())
+
+def parse(lines):
+    
+    support_tls13 = 1 if "tls13" in lines[7] else 0
+    metric = ["avg", "median", "min", "max", "75", "95", "99"]
+
+    ret = {
+        "support3": support_tls13,
+        "tls2": {
+            
+        }
+    }
+
+    for i in range(4, 24, 4):
+        ## TLS 1.2
+        ret["tls2"][lines[i][7:-1]] = {}
+        values = map(lambda a: float(a), lines[i+2][7:-1].split(","))
+        
+        for (index, value) in enumerate(values):
+            ret["tls2"][lines[i][7:-1]][metric[index]] = value
+        
+        ## TLS 1.3
+        if support_tls13:
+            ret["tls3"] = {}
+            ret["tls3"][lines[i][7:-1]] = {}
+            values = map(lambda a: float(a), lines[i+3][7:-1].split(","))
+            for (index, value) in enumerate(values):
+                ret["tls2"][lines[i][7:-1]][metric[index]] = value
+
+    return ret
 
 def main():
     print("Tool Started!!!")
     websites = []
     outputs = []
     readcsv(websites)
-    print(websites) #websites
+    #print(websites) #websites
     runcurl(websites,outputs)
-    print(outputs) #output of curl
+    #print(outputs) #output of curl
+    datamap = parse(outputs)
 
 if __name__ == "__main__":
     # execute only if run as a script
