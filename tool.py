e5bcc2ef9df9ef8456ee317d765c3f6422c0bf05
Leonard1874 <leonardwang2018@gmail.com>
2020-11-10 19:23:52 -0500

    ww:add-import

--- tool.py
+++ tool.py
@@ -1,7 +1,31 @@
 import os
+from parse import parse
 
 num = 3 #number of websites
-
+step = 100000 #step size
+stepNum = 10 #step number
+def readcsv(website):
+    # Using readline() 
+    file1 = open('./top-1m.csv', 'r') 
+    countStep = 0
+    
+    while countStep < stepNum:
+        for i in range (num):
+            # Get next line from file 
+            line = file1.readline() 
+            # if line is empty 
+            # end of file is reached 
+            if not line: 
+                break
+            lineSplit = line.split(',')
+            website.append(lineSplit[1])
+        for j in range (step - num):
+            file1.readline()
+            if not line: 
+                break
+        countStep += 1
+    file1.close()
+'''
 def readcsv(website):
     # Using readline() 
     file1 = open('./top-1m.csv', 'r') 
@@ -18,16 +42,18 @@ def readcsv(website):
         website.append(lineSplit[1])
         count += 1
     file1.close()
-
-def runcurl(websites, outputs):
+'''
+def runcurl(websites, outmaps):
     for website in websites:
         print(website)
         stream = os.popen('./curltimes.sh compare https://' + website)
-        #print(stream.read())
-        outputs.append(stream.read())
-
-def parse(lines):
-    
+        output = stream.read()
+        outmap = parse(output)
+        print(outmap)
+        outmaps.append(outmap)
+'''
+def parse(str):
+    lines = str.split("\n")
     support_tls13 = 1 if "tls13" in lines[7] else 0
     metric = ["avg", "median", "min", "max", "75", "95", "99"]
 
@@ -35,40 +61,35 @@ def parse(lines):
         "support3": support_tls13,
         "tls2": {
             
+        },
+        "tls3": {
+
         }
     }
 
     for i in range(4, 24, 4):
         ## TLS 1.2
-        ret["tls2"][lines[i][7:-1]] = {}
-        values = map(lambda a: float(a), lines[i+2][7:-1].split(","))
+        ret["tls2"][lines[i][7:]] = {}
+        values = map(lambda a: float(a), lines[i+2][7:].split(","))
         
         for (index, value) in enumerate(values):
-            ret["tls2"][lines[i][7:-1]][metric[index]] = value
+            ret["tls2"][lines[i][7:]][metric[index]] = value
         
         ## TLS 1.3
         if support_tls13:
-            ret["tls3"] = {}
-            ret["tls3"][lines[i][7:-1]] = {}
-            values = map(lambda a: float(a), lines[i+3][7:-1].split(","))
+            ret["tls3"][lines[i][7:]] = {}
+            values = map(lambda a: float(a), lines[i+3][7:].split(","))
             for (index, value) in enumerate(values):
-                ret["tls2"][lines[i][7:-1]][metric[index]] = value
-
+                ret["tls3"][lines[i][7:]][metric[index]] = value
     return ret
-
+'''
 def main():
     print("Tool Started!!!")
     websites = []
-    outputs = []
-    maps = []
+    outmaps = []
     readcsv(websites)
-    #print(websites) #websites
-    runcurl(websites,outputs)
-    #print(outputs) #output of curl
-    for output in outputs:
-        datamap = parse(outputs)
-        maps.append(datamap)
-    print(datamap)
+    print(websites) #websites
+    runcurl(websites,outmaps)
 
 if __name__ == "__main__":
     # execute only if run as a script
